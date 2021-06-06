7f0498a3a6d8978cd7e23f5782cc1ade9996b6a1
hihigupt <higupt@microsoft.com>
2021-06-06 16:37:06 -0700

    Add common utilities (#3)

---
+++ demos/common/utilities/crypto.h
@@ -0,0 +1,28 @@
+/* Copyright (c) Microsoft Corporation. All rights reserved. */
+/* SPDX-License-Identifier: MIT */
+
+#include <stdint.h>
+
+/**
+ * @brief Initialize crypto
+ * 
+ * @return An #uint32_t with result of operation.
+ */
+uint32_t Crypto_Init();
+
+/**
+ * @brief Compute HMAC SHA256
+ * 
+ * @param[in] pucKey Pointer to key.
+ * @param[in] ulKeyLength Length of Key.
+ * @param[in] pucData Pointer to data for HMAC
+ * @param[in] ulDataLength Length of data.
+ * @param[in,out] pucOutput Buffer to place computed HMAC.
+ * @param[out] ulOutputLength Length of output buffer.
+ * @param[in] pulBytesCopied Number of bytes copied to out buffer.
+ * @return An #uint32_t with result of operation.
+ */
+uint32_t Crypto_HMAC( const uint8_t * pucKey, uint32_t ulKeyLength,
+                      const uint8_t * pucData, uint32_t ulDataLength,
+                      uint8_t * pucOutput, uint32_t ulOutputLength,
+                      uint32_t * pulBytesCopied ); 
