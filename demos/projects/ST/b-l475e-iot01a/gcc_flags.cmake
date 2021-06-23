136d3cd64a881f22ed270cb5e12cbd90a637e7b4
Himanshu Gupta <higupt@microsoft.com>
2021-06-23 05:08:07 +0000

    [ST] Add gc of unuse section

--- demos/projects/ST/b-l475e-iot01a/gcc_flags.cmake
+++ demos/projects/ST/b-l475e-iot01a/gcc_flags.cmake
@@ -4,7 +4,9 @@
 set(MCU_C_FLAGS " \
     -mcpu=cortex-m4 \
     -mfpu=fpv4-sp-d16 \
-    -mfloat-abi=hard " CACHE INTERNAL "MCU build flags")
+    -mfloat-abi=hard -ffunction-sections" CACHE INTERNAL "MCU build flags")
 set(CMAKE_C_FLAGS ${CMAKE_C_FLAGS} ${MCU_C_FLAGS})
 
-set(CMAKE_EXE_LINKER_FLAGS ${CMAKE_EXE_LINKER_FLAGS} "-Wl,-Map=output.map")
+set(CMAKE_EXE_LINKER_FLAGS ${CMAKE_EXE_LINKER_FLAGS} "-Wl,--gc-sections \
+    -static -z muldefs -mthumb -Wl,--start-group -lc -lm -Wl,--end-group \
+    -Wl,-Map=output.map")
