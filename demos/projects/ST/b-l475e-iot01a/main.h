cad345510ada2cafb8ae812877f91fc231a8a40c
hihigupt <higupt@microsoft.com>
2021-07-02 15:14:58 -0700

    Update to newer copyright (#57)

--- demos/projects/ST/b-l475e-iot01a/main.h
+++ demos/projects/ST/b-l475e-iot01a/main.h
@@ -1,23 +1,5 @@
-/* USER CODE BEGIN Header */
-/**
-  ******************************************************************************
-  * @file           : main.h
-  * @brief          : Header for main.c file.
-  *                   This file contains the common defines of the application.
-  ******************************************************************************
-  * @attention
-  *
-  * <h2><center>&copy; Copyright (c) 2020 STMicroelectronics.
-  * All rights reserved.</center></h2>
-  *
-  * This software component is licensed by ST under BSD 3-Clause license,
-  * the "License"; You may not use this file except in compliance with the
-  * License. You may obtain a copy of the License at:
-  *                        opensource.org/licenses/BSD-3-Clause
-  *
-  ******************************************************************************
-  */
-/* USER CODE END Header */
+/* Copyright (c) Microsoft Corporation.
+   Licensed under the MIT License. */
 
 /* Define to prevent recursive inclusion -------------------------------------*/
 #ifndef __MAIN_H
