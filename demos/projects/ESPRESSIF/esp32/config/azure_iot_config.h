74562405ea4d83c871acfc049c9157e83daedd14
Himanshu Gupta <higupt@microsoft.com>
2021-08-17 01:11:05 +0000

    Fix indentations

--- demos/projects/ESPRESSIF/esp32/config/azure_iot_config.h
+++ demos/projects/ESPRESSIF/esp32/config/azure_iot_config.h
@@ -22,16 +22,16 @@
     #define LIBRARY_LOG_NAME    "AZ IOT"
 #endif
 
-#define SINGLE_PARENTESIS_LOGE(x, ...)    ESP_LOGE(LIBRARY_LOG_NAME, x, ##__VA_ARGS__)
+#define SINGLE_PARENTESIS_LOGE( x, ... )  ESP_LOGE( LIBRARY_LOG_NAME, x, ##__VA_ARGS__ )
 #define AZLogError( message )             SINGLE_PARENTESIS_LOGE message
 
-#define SINGLE_PARENTESIS_LOGI(x, ...)    ESP_LOGI(LIBRARY_LOG_NAME, x, ##__VA_ARGS__)
+#define SINGLE_PARENTESIS_LOGI( x, ... )  ESP_LOGI( LIBRARY_LOG_NAME, x, ##__VA_ARGS__ )
 #define AZLogInfo( message )              SINGLE_PARENTESIS_LOGI message
 
-#define SINGLE_PARENTESIS_LOGW(x, ...)    ESP_LOGW(LIBRARY_LOG_NAME, x, ##__VA_ARGS__)
+#define SINGLE_PARENTESIS_LOGW( x, ... )  ESP_LOGW( LIBRARY_LOG_NAME, x, ##__VA_ARGS__ )
 #define AZLogWarn( message )              SINGLE_PARENTESIS_LOGW message
 
-#define SINGLE_PARENTESIS_LOGD(x, ...)    ESP_LOGD(LIBRARY_LOG_NAME, x, ##__VA_ARGS__)
+#define SINGLE_PARENTESIS_LOGD( x, ... )  ESP_LOGD( LIBRARY_LOG_NAME, x, ##__VA_ARGS__ )
 #define AZLogDebug( message )             SINGLE_PARENTESIS_LOGD message
 
 /************ End of logging configuration ****************/
