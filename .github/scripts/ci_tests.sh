0f338b6608e35392b5466f2fb40a4aa8be48dc00
Ewerton Scaboro da Silva <ewertons@microsoft.com>
2021-08-24 15:58:06 -0700

    [ACR/2] Add github action to verify Espressif ESP32 sample

--- .github/scripts/ci_tests.sh
+++ .github/scripts/ci_tests.sh
@@ -10,6 +10,7 @@ set -o pipefail # Exit if pipe failed.
 TEST_SCRIPT_DIR=`dirname "$0"`
 TEST_FREERTOS_SRC=`pwd`/libs/FreeRTOS
 $TEST_SCRIPT_DIR/fetch_freertos.sh $TEST_FREERTOS_SRC
+RUN_ESPRESSIF_TESTS=0
 
 function exit_if_binary_does_not_exist()
 {
@@ -29,38 +30,65 @@ function sample_build() {
     local board=$2
     local outdir=$3
 
-    cmake -G Ninja -DBOARD=$board -DVENDOR=$vendor -B$outdir -DFREERTOS_PATH=$TEST_FREERTOS_SRC .
-    cmake --build $outdir
+    if [ $vendor == "ESPRESSIF" ]
+    then
+      idf.py build -C ./demos/projects/ESPRESSIF/esp32
+    else
+      cmake -G Ninja -DBOARD=$board -DVENDOR=$vendor -B$outdir -DFREERTOS_PATH=$TEST_FREERTOS_SRC .
+      cmake --build $outdir
+    fi
 }
 
-######################## Simulations #######################
+process_args ()
+{
+    for arg in $*
+    do
+      case "$arg" in
+          "-esp" | "--run-espressif-tests" ) RUN_ESPRESSIF_TESTS=1;;
+          * );;
+      esac
+    done
+}
 
-echo -e "::group::Building sample for linux port"
-sample_build "PC" "linux" "build_pc_linux"
-exit_if_binary_does_not_exist "build_pc_linux" "iot-middleware-sample"
-exit_if_binary_does_not_exist "build_pc_linux" "iot-middleware-sample-pnp"
+process_args $*
 
-######################## ST Boards #########################
+if [ $RUN_ESPRESSIF_TESTS == 0 ]
+then
+    ######################## Simulations #######################
 
-echo -e "::group::Building sample for ST b-l475e-iot01a port"
-sample_build "ST" "b-l475e-iot01a" "build_st_b-l475e-iot01a"
-exit_if_binary_does_not_exist "build_st_b-l475e-iot01a" "iot-middleware-sample.elf"
-exit_if_binary_does_not_exist "build_st_b-l475e-iot01a" "iot-middleware-sample-pnp.elf"
-exit_if_binary_does_not_exist "build_st_b-l475e-iot01a" "iot-middleware-sample-gsg.elf"
+    echo -e "::group::Building sample for linux port"
+    sample_build "PC" "linux" "build_pc_linux"
+    exit_if_binary_does_not_exist "build_pc_linux" "iot-middleware-sample"
+    exit_if_binary_does_not_exist "build_pc_linux" "iot-middleware-sample-pnp"
 
-echo -e "::group::Building sample for ST stm32h745i-disco port"
-sample_build "ST" "stm32h745i-disco" "build_st_stm32h745i-disco"
-exit_if_binary_does_not_exist "build_st_stm32h745i-disco" "iot-middleware-sample.elf"
-exit_if_binary_does_not_exist "build_st_stm32h745i-disco" "iot-middleware-sample-pnp.elf"
+    ######################## ST Boards #########################
 
-echo -e "::group::Building sample for ST b-l4s5i-iot01a port"
-sample_build "ST" "b-l4s5i-iot01a" "build_st_b-l4s5i-iot01a"
-exit_if_binary_does_not_exist "build_st_b-l4s5i-iot01a" "iot-middleware-sample.elf"
-exit_if_binary_does_not_exist "build_st_b-l4s5i-iot01a" "iot-middleware-sample-pnp.elf"
+    echo -e "::group::Building sample for ST b-l475e-iot01a port"
+    sample_build "ST" "b-l475e-iot01a" "build_st_b-l475e-iot01a"
+    exit_if_binary_does_not_exist "build_st_b-l475e-iot01a" "iot-middleware-sample.elf"
+    exit_if_binary_does_not_exist "build_st_b-l475e-iot01a" "iot-middleware-sample-pnp.elf"
+    exit_if_binary_does_not_exist "build_st_b-l475e-iot01a" "iot-middleware-sample-gsg.elf"
 
-######################## NXP Boards ########################
+    echo -e "::group::Building sample for ST stm32h745i-disco port"
+    sample_build "ST" "stm32h745i-disco" "build_st_stm32h745i-disco"
+    exit_if_binary_does_not_exist "build_st_stm32h745i-disco" "iot-middleware-sample.elf"
+    exit_if_binary_does_not_exist "build_st_stm32h745i-disco" "iot-middleware-sample-pnp.elf"
 
-echo -e "::group::Building sample for NXP mimxrt1060 port"
-sample_build "NXP" "mimxrt1060" "build_nxp_mimxrt1060"
-exit_if_binary_does_not_exist "build_nxp_mimxrt1060" "iot-middleware-sample.elf"
-exit_if_binary_does_not_exist "build_nxp_mimxrt1060" "iot-middleware-sample-pnp.elf"
+    echo -e "::group::Building sample for ST b-l4s5i-iot01a port"
+    sample_build "ST" "b-l4s5i-iot01a" "build_st_b-l4s5i-iot01a"
+    exit_if_binary_does_not_exist "build_st_b-l4s5i-iot01a" "iot-middleware-sample.elf"
+    exit_if_binary_does_not_exist "build_st_b-l4s5i-iot01a" "iot-middleware-sample-pnp.elf"
+
+    ######################## NXP Boards ########################
+
+    echo -e "::group::Building sample for NXP mimxrt1060 port"
+    sample_build "NXP" "mimxrt1060" "build_nxp_mimxrt1060"
+    exit_if_binary_does_not_exist "build_nxp_mimxrt1060" "iot-middleware-sample.elf"
+    exit_if_binary_does_not_exist "build_nxp_mimxrt1060" "iot-middleware-sample-pnp.elf"
+else
+    ######################## ESPRESSIF Boards ########################
+
+    echo -e "::group::Building sample for ESPRESSIF ESP32 port"
+    sample_build "ESPRESSIF" "esp32" "build"
+    exit_if_binary_does_not_exist "./demos/projects/ESPRESSIF/esp32/build" "azure_iot_freertos_esp32.bin"
+fi
