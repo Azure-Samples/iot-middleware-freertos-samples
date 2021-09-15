170e6b2cda497a0c5d7d62e9dbb3aca93824eac5
Ewerton Scaboro da Silva <ewertons@microsoft.com>
2021-09-15 08:57:57 -0700

    Add ESP32 Azure IoT Kit sample to gate build

--- .github/scripts/ci_tests.sh
+++ .github/scripts/ci_tests.sh
@@ -37,6 +37,7 @@ function sample_build() {
     if [ $vendor == "ESPRESSIF" ]
     then
       idf.py build -C ./demos/projects/ESPRESSIF/esp32
+      idf.py build -C ./demos/projects/ESPRESSIF/aziotkit
     else
       cmake -G Ninja -DBOARD=$board -DVENDOR=$vendor -B$outdir -DFREERTOS_PATH=$TEST_FREERTOS_SRC .
       cmake --build $outdir
@@ -59,6 +60,7 @@ do
             echo -e "::group::Building sample for ESPRESSIF ESP32 port"
             sample_build "ESPRESSIF" "esp32" "build"
             exit_if_binary_does_not_exist "./demos/projects/ESPRESSIF/esp32/build" "azure_iot_freertos_esp32.bin"
+            exit_if_binary_does_not_exist "./demos/projects/ESPRESSIF/aziotkit/build" "azure_iot_freertos_esp32.bin"
             ;;
         "-nxp")
             fetch_freertos
