731b7969c7278e8f33d08ce73c87de4c029c00d5
Dane Walton <dawalton@microsoft.com>
2022-03-17 09:50:50 -0700

    add core http

---
+++ libs/azure-iot-middleware-freertos/ports/coreHTTP/azure_iot_core_http.c
@@ -0,0 +1,21 @@
+
+#include "azure_iot_http.h"
+
+#include "coreHTTP.h"
+
+
+uint32_t ulAzureIoTHTTP_Init( AzureIoTHTTPHandle_t xHTTPHandle,
+                              const char * pucURL )
+{
+  
+}
+
+uint32_t ulAzureIoTHTTP_Request( AzureIoTHTTPHandle_t xHTTPHandle )
+{
+
+}
+
+uint32_t ulAzureIoTHTTP_Deinit( AzureIoTHTTPHandle_t xHTTPHandle )
+{
+
+}
