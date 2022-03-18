fc2e0510a9d61f570db6f22e22869713e0f55e64
Dane Walton <dawalton@microsoft.com>
2022-03-18 16:31:00 -0700

    working compilation with corehttp

--- libs/azure-iot-middleware-freertos/ports/coreHTTP/azure_iot_core_http.c
+++ libs/azure-iot-middleware-freertos/ports/coreHTTP/azure_iot_core_http.c
@@ -1,21 +1,28 @@
 
 #include "azure_iot_http.h"
 
-#include "coreHTTP.h"
+#include "core_http_client.h"
 
 
 uint32_t ulAzureIoTHTTP_Init( AzureIoTHTTPHandle_t xHTTPHandle,
                               const char * pucURL )
 {
-  
+  (void)xHTTPHandle;
+  (void)pucURL;
+
+  return 0;
 }
 
 uint32_t ulAzureIoTHTTP_Request( AzureIoTHTTPHandle_t xHTTPHandle )
 {
+  (void)xHTTPHandle;
 
+  return 0;
 }
 
 uint32_t ulAzureIoTHTTP_Deinit( AzureIoTHTTPHandle_t xHTTPHandle )
 {
+  (void)xHTTPHandle;
 
+  return 0;
 }
