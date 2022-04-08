0de23dc09c301e8dc4e46f3ec483277130456884
Dane Walton <dawalton@microsoft.com>
2022-04-08 13:10:32 -0400

    add linux port

---
+++ demos/projects/PC/linux/port/azure_iot_flash_platform_port.h
@@ -0,0 +1,21 @@
+/* Copyright (c) Microsoft Corporation.
+ * Licensed under the MIT License. */
+
+/**
+ * @file azure_iot_flash_platform_port.h
+ * 
+ * @brief Port file for the linux flash abstraction.
+ * 
+ */
+
+#ifndef AZURE_IOT_FLASH_PLATFORM_PORT_H
+#define AZURE_IOT_FLASH_PLATFORM_PORT_H
+
+typedef struct AzureADUImageContext
+{
+    void * unused
+} AzureADUImageContext_t;
+
+typedef AzureADUImageContext_t AzureADUImage_t;
+
+#endif // AZURE_IOT_FLASH_PLATFORM_PORT_H
