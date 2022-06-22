b171ba42a1f0386db34cd8c9f5f10da908a12a95
Dane Walton <dawalton@microsoft.com>
2022-06-21 19:12:37 -0700

    change name of tests

--- .github/scripts/ci_tests.sh
+++ .github/scripts/ci_tests.sh
@@ -101,7 +101,7 @@ do
             exit_if_binary_does_not_exist "build_pc_linux" "iot-middleware-sample-pnp"
 
             echo -e "::group::Running manifest verification tests"
-            ./build_pc_linux/demos/projects/PC/linux/sample_adu_jws_mbedtls_ut
+            ./build_pc_linux/demos/projects/PC/linux/sample_adu_jws_mbedtls_int_test
             ;;
         * )
             echo "build for $arg not found";;
