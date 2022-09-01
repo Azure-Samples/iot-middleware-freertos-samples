dd607abea068c7752e9430a7bd48e3fc75bf518d
Dane Walton <dawalton@microsoft.com>
2022-09-01 10:50:29 -0500

    add to esp

--- demos/projects/ESPRESSIF/adu/main/azure_iot_freertos_esp32_pnp_model.c
+++ demos/projects/ESPRESSIF/adu/main/azure_iot_freertos_esp32_pnp_model.c
@@ -299,10 +299,14 @@ void vHandleWritableProperties( AzureIoTHubClientPropertiesResponse_t * pxMessag
             }
             else if( xAzureIoTAduUpdateRequest.xWorkflow.xAction == eAzureIoTADUActionCancel )
             {
+                xProcessUpdateRequest = false;
+
                 LogInfo( ( "ADU manifest received: action cancelled" ) );
             }
             else
             {
+                xProcessUpdateRequest = false;
+
                 LogInfo( ( "ADU manifest received: action unknown" ) );
             }
         }
