a0fd34a1a93f0717677a3dcedf604c3939b11b52
Ewerton Scaboro da Silva <ewertons@microsoft.com>
2022-09-12 09:32:18 -0700

    Change ADU client update id to match service specification
    Change ADU client update id to match service specification
    Please refer to https://github.com/Azure/azure-sdk-for-c/pull/2331 for details.

--- demos/sample_azure_iot_adu/sample_azure_iot_adu.c
+++ demos/sample_azure_iot_adu/sample_azure_iot_adu.c
@@ -128,6 +128,8 @@
  */
 #define ADU_HEADER_BUFFER_SIZE                                512
 
+#define democonfigADU_UPDATE_ID                               "{\"provider\":\"" democonfigADU_UPDATE_PROVIDER "\",\"name\":\"" democonfigADU_UPDATE_NAME "\",\"version\":\"" democonfigADU_UPDATE_VERSION "\"}"
+
 /*-----------------------------------------------------------*/
 
 /**
@@ -162,15 +164,8 @@ AzureIoTADUClientDeviceProperties_t xADUDeviceProperties =
     .ulManufacturerLength                     = sizeof( democonfigADU_DEVICE_MANUFACTURER ) - 1,
     .ucModel                                  = ( const uint8_t * ) democonfigADU_DEVICE_MODEL,
     .ulModelLength                            = sizeof( democonfigADU_DEVICE_MODEL ) - 1,
-    .xCurrentUpdateId                         =
-    {
-        .ucProvider                           = ( const uint8_t * ) democonfigADU_UPDATE_PROVIDER,
-        .ulProviderLength                     = sizeof( democonfigADU_UPDATE_PROVIDER ) - 1,
-        .ucName                               = ( const uint8_t * ) democonfigADU_UPDATE_NAME,
-        .ulNameLength                         = sizeof( democonfigADU_UPDATE_NAME ) - 1,
-        .ucVersion                            = ( const uint8_t * ) democonfigADU_UPDATE_VERSION,
-        .ulVersionLength                      = sizeof( democonfigADU_UPDATE_VERSION ) - 1
-    },
+    .ucCurrentUpdateId                        = ( const uint8_t * ) democonfigADU_UPDATE_ID,
+    .ulCurrentUpdateIdLength                  = sizeof( democonfigADU_UPDATE_ID ) - 1,
     .ucDeliveryOptimizationAgentVersion       = NULL,
     .ulDeliveryOptimizationAgentVersionLength = 0
 };
@@ -645,7 +640,7 @@ static AzureIoTResult_t prvSpoofNewVersion( void )
         LogError( ( "[ADU] New ADU update version for simulator not given." ) );
     #endif
     LogInfo( ( "[ADU] Device Version %.*s",
-               xADUDeviceProperties.xCurrentUpdateId.ulVersionLength, xADUDeviceProperties.xCurrentUpdateId.ucVersion ) );
+               xADUDeviceProperties.ulCurrentUpdateIdLength, xADUDeviceProperties.ucCurrentUpdateId ) );
     return AzureIoTADUClient_SendAgentState( &xAzureIoTADUClient,
                                              &xAzureIoTHubClient,
                                              &xADUDeviceProperties,
