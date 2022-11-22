ee26be8ff24475d12baf7dee3dd69be7e937abf9
Dane Walton <dawalton@microsoft.com>
2022-11-22 16:06:48 -0500

    use large buffer regardless of dps

--- demos/projects/ST/b-l4s5i-iot01a/config/demo_config.h
+++ demos/projects/ST/b-l4s5i-iot01a/config/demo_config.h
@@ -510,11 +510,7 @@ static unsigned char root_cert_array[] = {
  */
 #define WIFI_SECURITY_TYPE  WIFI_ECN_WPA2_PSK
 
-#ifdef democonfigENABLE_DPS_SAMPLE
-    #define democonfigCHUNK_DOWNLOAD_SIZE    256
-#else
-    #define democonfigCHUNK_DOWNLOAD_SIZE    1024
-#endif /* democonfigENABLE_DPS_SAMPLE */
+#define democonfigCHUNK_DOWNLOAD_SIZE    4096
 
 #define democonfigADU_DEVICE_MANUFACTURER    "STMicroelectronics"
 #define democonfigADU_DEVICE_MODEL           "STM32L4S5I"
