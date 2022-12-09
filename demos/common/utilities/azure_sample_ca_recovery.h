4732682d37d39363a18276f753886e24a2cf7684
Dane Walton <dawalton@microsoft.com>
2022-12-09 15:22:03 -0500

    code style

--- demos/common/utilities/azure_sample_ca_recovery.h
+++ demos/common/utilities/azure_sample_ca_recovery.h
@@ -6,36 +6,38 @@
 #include "azure_iot_result.h"
 #include "azure_iot_json_reader.h"
 
-typedef struct AzureIoTCARecovery_TrustBundle {
-  const uint8_t * pucVersion;
-  uint32_t ulVersionLength;
+typedef struct AzureIoTCARecovery_TrustBundle
+{
+    const uint8_t * pucVersion;
+    uint32_t ulVersionLength;
 
-  const uint8_t * pucExpiryTime;
-  uint32_t ulExpiryTimeLength;
+    const uint8_t * pucExpiryTime;
+    uint32_t ulExpiryTimeLength;
 
-  const uint8_t * pucCertificates;
-  uint32_t ulCertificatesLength;
+    const uint8_t * pucCertificates;
+    uint32_t ulCertificatesLength;
 } AzureIoTCARecovery_TrustBundle;
 
-typedef struct AzureIoTCARecovery_RecoveryPayload {
-  const uint8_t * pucIoTHubHostname;
-  uint32_t ulIoTHubHostnameLength;
+typedef struct AzureIoTCARecovery_RecoveryPayload
+{
+    const uint8_t * pucIoTHubHostname;
+    uint32_t ulIoTHubHostnameLength;
 
-  const uint8_t * pucPayloadSignature;
-  uint32_t ulPayloadSignatureLength;
+    const uint8_t * pucPayloadSignature;
+    uint32_t ulPayloadSignatureLength;
 
-  const uint8_t * pucTrustBundleJSONObjectText;
-  uint32_t ulTrustBundleJSONObjectTextLength;
+    const uint8_t * pucTrustBundleJSONObjectText;
+    uint32_t ulTrustBundleJSONObjectTextLength;
 
-  AzureIoTCARecovery_TrustBundle xTrustBundle;
+    AzureIoTCARecovery_TrustBundle xTrustBundle;
 } AzureIoTCARecovery_RecoveryPayload;
 
 /**
  * @brief Parse the recovery payload sent from the Device Provisioning Service.
- * 
+ *
  * @param[in,out] pxReader The JSON reader initialized with the payload.
  * @param[out] pxRecoveryPayload The struct into which the payload will be parsed.
  * @return AzureIoTResult_t
  */
-AzureIoTResult_t AzureIoTCARecovery_ParseRecoveryPayload(AzureIoTJSONReader_t * pxReader,
-                                                AzureIoTCARecovery_RecoveryPayload * pxRecoveryPayload);
+AzureIoTResult_t AzureIoTCARecovery_ParseRecoveryPayload( AzureIoTJSONReader_t * pxReader,
+                                                          AzureIoTCARecovery_RecoveryPayload * pxRecoveryPayload );
