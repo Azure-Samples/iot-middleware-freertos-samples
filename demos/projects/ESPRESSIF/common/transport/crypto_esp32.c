99e81670f4f3dcc6f8ab436ec72917407183a7c7
Dane Walton <dawalton@microsoft.com>
2023-03-01 16:10:06 -0500

    move transport code

--- demos/projects/ESPRESSIF/common/transport/crypto_esp32.c
+++ demos/projects/ESPRESSIF/common/transport/crypto_esp32.c
@@ -1,5 +1,5 @@
 /* Copyright (c) Microsoft Corporation.
-   Licensed under the MIT License. */
+ * Licensed under the MIT License. */
 
 #include "azure_sample_crypto.h"
 
@@ -15,9 +15,12 @@ uint32_t Crypto_Init()
 }
 /*-----------------------------------------------------------*/
 
-uint32_t Crypto_HMAC( const uint8_t * pucKey, uint32_t ulKeyLength,
-                      const uint8_t * pucData, uint32_t ulDataLength,
-                      uint8_t * pucOutput, uint32_t ulOutputLength,
+uint32_t Crypto_HMAC( const uint8_t * pucKey,
+                      uint32_t ulKeyLength,
+                      const uint8_t * pucData,
+                      uint32_t ulDataLength,
+                      uint8_t * pucOutput,
+                      uint32_t ulOutputLength,
                       uint32_t * pulBytesCopied )
 {
     uint32_t ulRet;
